a99d00f50c910f2ca32db673dc36e5eea65d40ee
sundeeseeyou <sandiseeyou@gmail.com>
2024-09-07 19:47:23 +0700

    add livereload

---
+++ src/components/Features.jsx
@@ -0,0 +1,11 @@
+import { Typography } from "@material-tailwind/react";
+
+export default function Features() {
+  return (
+    <>
+      <Typography variant="h2" className="text-primeColor">
+        Why Acronis?
+      </Typography>
+    </>
+  );
+}
