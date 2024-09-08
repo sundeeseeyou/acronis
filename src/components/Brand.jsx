2e28c351f0870fe172a65d27213251f4c3f9f9ef
sundeeseeyou <sandiseeyou@gmail.com>
2024-09-08 13:32:13 +0700

    Fix gallery look secton

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -1,1 +1,4 @@
+import GoogleIcon from "../assets/Acronis/Brand/google.png";
+import { Typography } from "@material-tailwind/react";
+
 export default function Brand() {}
--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,4 +1,5 @@
-import { Typography } from "@material-tailwind/react";
+import { Card, CardBody, Typography, Avatar } from "@material-tailwind/react";
+import BannerImg from "../assets/Acronis/Banner.jpg";
 
 export default function Features() {
   return (
@@ -17,13 +18,34 @@ export default function Features() {
           here to make life better.
         </Typography>
 
-        <div className="grid grid-cols-4 my-8 gap-4">
-          <div className="bg-blue-900 text-white flex justify-center items-center p-6 rounded-xl col-span-3 row-span-1 h-64 lg:h-96">
-            Caption 1
-          </div>
-          <div className="bg-yellow-500 text-white flex justify-center items-center p-6 rounded-xl col-span-1 row-span-1 h-64 lg:h-96">
+        {/* Grid Items */}
+
+        <div className="grid grid-cols-4 my-8 gap-4 overflow-hidden">
+          <Card className="w-full flex justify-center items-center col-span-3 row-span-1 h-64 lg:h-96 rounded-2xl">
+            <img
+              src={BannerImg}
+              alt="Banner"
+              className="w-full object-cover object-center h-full rounded-2xl"
+            />
+            <CardBody className="absolute w-full inset-0 flex flex-col justify-end items-center text-left px-10 py-10">
+              <Typography
+                variant="h3"
+                className="w-full text-white lg:text-4xl text-base self-center"
+              >
+                All in One Software
+              </Typography>
+              <Typography
+                variant="paragraph"
+                className="text-white text-lg w-full"
+              >
+                You only need one software to do multiple jobs and different
+                tasks
+              </Typography>
+            </CardBody>
+          </Card>
+          <Card className="text-black text-4xl flex justify-center items-center p-6 rounded-xl col-span-1 row-span-1 h-64 lg:h-96">
             Caption 2
-          </div>
+          </Card>
 
           <div className="bg-blue-900 text-white flex justify-center items-center p-6 rounded-xl col-span-1 row-span-1 h-64 lg:h-96">
             Caption 3
