601984a54398a5096a073e9a98e2c56911cdf377
sundeeseeyou <sandiseeyou@gmail.com>
2024-09-08 10:00:47 +0700

    Fixing base vite

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import liveReload from "vite-plugin-live-reload";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), liveReload("**/*")],
+  base: "/website2",
   server: {
     watch: {
       usePolling: true,
